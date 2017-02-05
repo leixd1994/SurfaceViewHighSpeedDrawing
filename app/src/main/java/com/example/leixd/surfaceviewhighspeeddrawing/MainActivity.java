a2feed854de6571cf52fe5ade07fbfcd6168b1ce
Leixd1994 <929650619@qq.com>
2017-02-05 17:44:30 +0800

    变为了真正的SurfaceView，使用缓冲技术绘制背景网格

--- app/src/main/java/com/example/leixd/surfaceviewhighspeeddrawing/MainActivity.java
+++ app/src/main/java/com/example/leixd/surfaceviewhighspeeddrawing/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,55 +15,55 @@ public class MainActivity extends AppCompatActivity {
 
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        LayoutInflater layoutInflater=LayoutInflater.from(this);
 
-        final SurfaceView surfaceView= (SurfaceView)findViewById(R.id.surfaceview);
-        surfaceView.init(metrics);
-        surfaceView.setLineSize(5);
-        final Handler handler=new Handler(){
-            @Override
-            public void handleMessage(Message msg) {
 
-                switch (msg.what)
-                {
-                    case 1:
-                        float []t=new float[5];
-                        for (int t0=0;t0<5;t0++)
-                        {
-                            t[t0]=114;
-
-                        }
-                        surfaceView.update(t)
-                        ;break;
-                    case 2:
-                        float []t3=new float[5];
-                        for (int t0=0;t0<5;t0++)
-                        {
-                            t3[t0]=414;
-
-                        }
-                        surfaceView.update(t3)
-                        ;break;
-
-                }
-
-                super.handleMessage(msg);
-            }
-        };
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    handler.sendEmptyMessage(1);
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    handler.sendEmptyMessage(2);
-                }
-            }
-        }).start();
+        final MySurfaceView mySurfaceView = (MySurfaceView)findViewById(R.id.surfaceview);
+        mySurfaceView.init(metrics,800,800);
+        mySurfaceView.setLineSize(5);              //设置线宽
+//        final Handler handler=new Handler(){
+//            @Override
+//            public void handleMessage(Message msg) {
+//
+//                switch (msg.what)
+//                {
+//                    case 1:
+//                        float []t=new float[5];
+//                        for (int t0=0;t0<5;t0++)
+//                        {
+//                            t[t0]=114;
+//
+//                        }
+//                        mySurfaceView.update(t)
+//                        ;break;
+//                    case 2:
+//                        float []t3=new float[5];
+//                        for (int t0=0;t0<5;t0++)
+//                        {
+//                            t3[t0]=414;
+//
+//                        }
+//                        mySurfaceView.update(t3)
+//                        ;break;
+//
+//                }
+//
+//                super.handleMessage(msg);
+//            }
+//        };
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true) {
+//                    handler.sendEmptyMessage(1);
+//                    try {
+//                        Thread.sleep(200);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    handler.sendEmptyMessage(2);
+//                }
+//            }
+//        }).start();
 
 
     }
--- app/src/main/java/com/example/leixd/surfaceviewhighspeeddrawing/MySurfaceView.java
+++ app/src/main/java/com/example/leixd/surfaceviewhighspeeddrawing/MySurfaceView.java
@@ -1,6 +1,8 @@
 package com.example.leixd.surfaceviewhighspeeddrawing;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,6 +11,8 @@ import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,7 +20,8 @@ import android.widget.Toast;
  * Created by 雷晓东 on 17-1-30.
  */
 
-public class SurfaceView extends View {
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder surfaceHolder;
     private Paint paint,paintGreen;
     private Path path;
     private DisplayMetrics metrics;             //屏幕相关的数据
@@ -24,17 +29,18 @@ public class SurfaceView extends View {
     private float pxBetweenPoint;               //点之间的间隔像素
     private float []TotalPoint2;    
     private int currentsite=0;                  //当前位置
-    public SurfaceView(Context context)
-    {
-        super(context);
-    }
-    public SurfaceView(Context context, AttributeSet attributeSet)
+    Bitmap backgroundbitmap;             //背景Bitmap
+    private Canvas canvas,surfacecanvas;
+
+    public MySurfaceView(Context context, AttributeSet attributeSet)
     {
         super(context,attributeSet);
+        surfaceHolder=getHolder();
+        surfaceHolder.addCallback(this);
 
     }
 
-    public void init(DisplayMetrics metrics)            //得到metrics并初始化控件
+    public void init(DisplayMetrics metrics,int weight,int height)            //得到metrics并初始化控件
     {
         paint=new Paint();
         paint.setColor(Color.WHITE);
@@ -42,11 +48,8 @@ public class SurfaceView extends View {
         paintGreen.setColor(Color.GREEN);
         this.metrics=metrics;
         TotalPoint2=new float[TotalPoint];
-     /*   for (int t=0;t<600;t++)
-        {
-            TotalPoint2[t]=124;
-        }
-*/
+
+
     }
     public Boolean setLineSize(float size)   //设置线宽
     {
@@ -56,18 +59,23 @@ public class SurfaceView extends View {
 
     public Boolean update(float []point)            //更新心电图的部分
     {
-        int t=0;
-        while (t<point.length) {
-            TotalPoint2[currentsite++] = point[t++];
-            if (currentsite>=TotalPoint) currentsite=0;
-        }
-          invalidate();
+
         return true;
     }
+
+
     @Override
-    protected void onDraw(Canvas canvas) {
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        backgroundbitmap=Bitmap.createBitmap(this.getWidth(),this.getHeight(), Bitmap.Config.ARGB_8888);
+        for (int t=0;t<600;t++)
+        {
+            TotalPoint2[t]=124;
+        }
+
+        canvas=new Canvas();
+        canvas.setBitmap(backgroundbitmap);
         canvas.drawColor(Color.BLACK);  //绘制黑色背景
-       while(metrics==null);   //等待主函数传入metrics
+        while(metrics==null);   //等待主函数传入metrics
 
         final float test= TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM,5,metrics);  //获得5mm对应的像素数量
         float drawy=0;
@@ -80,15 +88,21 @@ public class SurfaceView extends View {
             canvas.drawLine(drawy, 0, drawy, canvas.getHeight(), paint);
             drawy=drawy+test;
         }
-            super.onDraw(canvas);
+        surfacecanvas=surfaceHolder.lockCanvas();
+        surfacecanvas.drawBitmap(backgroundbitmap,0,0,paint);
+        surfaceHolder.unlockCanvasAndPost(surfacecanvas);
+
         //以下为绘制心电图部分//////
         pxBetweenPoint=(float)canvas.getWidth()/TotalPoint;     //初始化点之间的间隔
-        path=new Path();
-        path.moveTo(0,test);
-        int currentdrawsite=0;   //当前绘图位置
-        while (currentdrawsite<TotalPoint2.length-1) {
-           canvas.drawLine(currentdrawsite*pxBetweenPoint,TotalPoint2[currentdrawsite],++currentdrawsite*pxBetweenPoint,TotalPoint2[currentdrawsite],paintGreen);
-        }
+    }
 
-        }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
 }
